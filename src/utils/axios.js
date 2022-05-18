480f2396bf49a2d35254f2f8dea20f93c512a4f8
Fish Walker <59117535+linhfishCR7@users.noreply.github.com>
2022-05-18 13:07:08 +0700

    change url

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,5 +1,6 @@
 import axios from 'axios'
 
 export default axios.create({
-  baseURL: 'http://52.77.241.172:8000/api',
+  // baseURL: 'http://52.77.241.172:8000/api',
+  baseURL: 'https://api.havanlinh.tech:8000/api',
 })
