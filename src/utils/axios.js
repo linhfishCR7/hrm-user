c9482699bf74b86a4e35e288abbc770a8ef37c72
Fish Walker <59117535+linhfishCR7@users.noreply.github.com>
2022-05-18 12:48:44 +0700

    chang url api

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,5 +1,7 @@
 import axios from 'axios'
 
 export default axios.create({
-  baseURL: 'http://localhost:8000/api',
+  // baseURL: 'http://localhost:8000/api',
+  baseURL: 'http://api.havanlinh.tech:8000/api',
+
 })
